0944a22c99b657a34d89bdf50600293d5947da80
Kanishk Kaushal <75947299+Kanishk-Kaushal@users.noreply.github.com>
2022-01-17 22:44:15 +0530

    Create Runsim.m

---
+++ Graph Based Path Planning/Dijkstra Algorithm/Runsim.m
@@ -0,0 +1,20 @@
+%
+% TestScript for A Star Algorithm
+%
+
+%% Define a small map
+map = false(10);
+
+% Add an obstacle
+map (1:5, 6) = true;
+
+start_coords = [6, 2];
+dest_coords  = [8, 9];
+
+%%
+close all;
+
+
+[route, numExpanded] = DijkstraGrid (map, start_coords, dest_coords);
+
+
